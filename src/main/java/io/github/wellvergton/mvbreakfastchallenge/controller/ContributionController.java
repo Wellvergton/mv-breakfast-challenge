af26c99b71e065d7e2d3b4c96c9b594b72b03850
Wellvergton Marques <wmc.jp@outlook.com>
2021-10-22 14:16:13 -0300

    Improved cors configuration.

--- src/main/java/io/github/wellvergton/mvbreakfastchallenge/controller/ContributionController.java
+++ src/main/java/io/github/wellvergton/mvbreakfastchallenge/controller/ContributionController.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,7 +18,6 @@ import org.springframework.web.bind.annotation.RestController;
 import io.github.wellvergton.mvbreakfastchallenge.model.Contribution;
 import io.github.wellvergton.mvbreakfastchallenge.repository.ContributionRepository;
 
-@CrossOrigin
 @RestController
 @RequestMapping("/contribution")
 public class ContributionController {
--- src/main/java/io/github/wellvergton/mvbreakfastchallenge/controller/EmployeeController.java
+++ src/main/java/io/github/wellvergton/mvbreakfastchallenge/controller/EmployeeController.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,7 +17,6 @@ import org.springframework.web.bind.annotation.RestController;
 import io.github.wellvergton.mvbreakfastchallenge.model.Employee;
 import io.github.wellvergton.mvbreakfastchallenge.repository.EmployeeRepository;
 
-@CrossOrigin
 @RestController
 @RequestMapping("/employee")
 public class EmployeeController {
